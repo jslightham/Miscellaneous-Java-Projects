37f169928f4fc5d11cdbf4c3ac192a41d8dc9905
jslightham <31053827+jslightham@users.noreply.github.com>
2018-10-02 23:52:02 -0400

    MySql testing

---
+++ MySQL Test/src/mySQL/Main.java
@@ -0,0 +1,23 @@
+package mySQL;
+
+import java.sql.*;
+
+public class Main {
+	public static void main(String[] args) {
+	try {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery("SELECT * FROM testtable");
+		
+		while(resultSet.next()){
+			System.out.println(resultSet.getInt(1) + "  " + resultSet.getInt(2)+ "  " + resultSet.getInt(3) + "  " + resultSet.getInt(3));  
+			
+		}
+		
+	} 	
+	catch (Exception e) {
+	System.out.println(e);
+	}
+	}
+}
